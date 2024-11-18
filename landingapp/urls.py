9dc8c085cdf89b4f603cb5f5e0f7e5dc391f9122
Hyunji An <hyunji0322123@gmail.com>
2024-11-17 20:24:59 -0800

    front

--- landingapp/urls.py
+++ landingapp/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('map/', views.map_page, name='map_page'),
+    path('map/', views.map_page, name='map'),
 ]
--- landingapp/views.py
+++ landingapp/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'landingapp/home.html')
+    return render(request, 'home.html')
 
 def about(request):
-    return render(request, 'landingapp/about.html')
+    return render(request, 'about.html')
 
 def map_page(request):
-    return render(request, 'landingapp/map.html')
+    return render(request, 'map.html')
 
 
